b8e7eaff9da874b889b65e8e8288b0511b828915
Joshua Boggs <joshua.j.boggs@gmail.com>
2014-05-01 14:30:50 -0400

    Added large cube target and shaders

---
+++ UnityProject/Assets/GameLogic.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameLogic : MonoBehaviour {
+
+	bool basketball = false;
+	bool mole = false;
+	bool hammer = false;
+	bool shooting = false;
+	public GameObject Carnival;
+	public GameObject Hoop;
+	public GameObject Rim;
+	public GameObject Ball;
+
+	// Use this for initialization
+	void Start () {
+		Carnival = GameObject.Find("Carnival");
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+	void OnGUI () {
+	
+	}
+}
---
+++ UnityProject/Assets/mover.js
@@ -0,0 +1,31 @@
+﻿#pragma strict
+
+var right = 1;
+var left = 0;
+
+function Start () {
+
+}
+
+function Update () {
+
+ 	if (this.transform.position.x < 770.0f && right==1) {
+ 		transform.Translate(Vector3.right * Time.deltaTime * 500);
+ 	}
+ 	
+ 	if (this.transform.position.x > 770.0f) {
+ 		right = 0;
+ 		left = 1;
+ 	}
+ 	
+ 	if (this.transform.position.x > -770.0f && left==1) {
+ 		transform.Translate(Vector3.right * Time.deltaTime * -500);
+ 	}
+ 	
+ 	if (this.transform.position.x < -770.0f) {
+ 		left = 0;
+ 		right = 1;
+ 	}
+ 	
+ 	transform.position.x = Mathf.Clamp(transform.position.x, -775, 775);
+}

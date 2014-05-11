6bf7dfd24ad5cca03405d795c72c12bcc7a9b017
Isaac White <iaw2105@columbia.edu>
2014-05-10 21:09:00 -0400

    Beginning of working game

---
+++ UnityProject/Assets/hammer_reporter.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class hammer_reporter : MonoBehaviour {
+
+	delegate void MyDelegate(string name);
+	MyDelegate mydelegate;
+	// Use this for initialization
+	void Start () {
+		GameObject delegateObject = GameObject.Find ("whack_a_mole");
+		mole_behavior otherScript = (mole_behavior) delegateObject.GetComponent(typeof(mole_behavior));
+		mydelegate = otherScript.reportCollidedWith;
+		             
+
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnCollisionEnter(Collision collision) {
+		foreach (ContactPoint contact in collision.contacts) {
+				print (contact.otherCollider.name);
+		}
+	}
+
+	void OnTriggerEnter(Collider other) {
+		mydelegate(other.name);
+	}
+}
--- UnityProject/Assets/mole_behavior.cs
+++ UnityProject/Assets/mole_behavior.cs
@@ -5,36 +5,34 @@ using System.Collections.Generic;
 public class mole_behavior : MonoBehaviour {
 	
 	private GameObject[][] moles;
+	private GameObject hammer;
+	private GameObject startButton;
 	private float maxY;
 	private float minY;
 	private GameObject aMole;
+	private TextMesh yourScoreboard;
+	private TextMesh highScoreboard;
 	private ArrayList jumpingMoles;
 	private Dictionary<string,int> isJumping;
+	private Dictionary<string,Vector3> targets;
+	private Queue<string> molesToWhack;
 	private int jumpThreshold;
-//	private abstract[]
-//	private ArrayList telephoneBooths;
-//	private ArrayList airplanes;
-//	private ArrayList subways;
-//	private GameObject airplane;
-//	private GameObject telephoneBooth;
-//	private GameObject subway;
-//	private GameObject car;
-//	private GameObject target;
-//	private Vector3 location;
-//	private TextMesh modeLabel;
-//	private TextMesh scaleFactor;
-//	private TextMesh warning;
-//	private double scale;
-//	private GameObject selected;
-//	private int count;
-//	private string[] modes;
-//	private int activeMode;
-//	private bool confirmed;
-	// Use this for initialization
+	private int maxMoles;
+	public bool letsPlay;
+	private bool reset;
+	private Vector3 currentTarget;
+	private float hammerSpeed;
+	private int hammerDirection;
+	private float strikingSpeed;
+	private int score;
+	private int hits;
+	private int jumpedCount;
+	private int highscore;
 	void Start () {
 		
 	}
 	void Awake() {
+//		letsPlay = true;
 		//we could do something more concise than this, but this is faster for now.
 		moles = new GameObject[3][];
 		moles [0] = new GameObject[4];
@@ -57,70 +55,99 @@ public class mole_behavior : MonoBehaviour {
 
 
 		aMole = moles [0] [0];
-		maxY = aMole.transform.position.y;
-		jumpThreshold = 90;
+		hammer = GameObject.Find ("mole_hammer");
+		highScoreboard = (TextMesh) GameObject.Find ("high_score").GetComponent (typeof(TextMesh));;
+		yourScoreboard = (TextMesh) GameObject.Find ("your_score").GetComponent (typeof(TextMesh));;
+		startButton = GameObject.Find ("lets_play");
+		maxY = aMole.transform.localPosition.y;
+		print (maxY);
+		jumpThreshold = 92;
 		jumpingMoles = new ArrayList();
-
-		minY = maxY - 100.0f;
+		maxMoles = 6;
+		hammerSpeed = 15.0f;
+		strikingSpeed = 20.0f;
+		minY = maxY - 150.0f;
 		isJumping = new Dictionary<string,int>(); //assign the dictionary.
+		targets = new Dictionary<string,Vector3> ();
+		jumpedCount = 0;
+		score = 0;
+		hits = 0;
 
 		//now set up the values
 		//would this be better if we used the object names instead?
-		for(int j = 0; j< moles.Length; j++) {
-			GameObject[] row = moles[j];
-			for(int i = 0; i < row.Length; i++) {
-				//instead of using the row count, let's switch to mole name. This should be easier to check against.
-//				string hashIndex = j.ToString () + i.ToString ();
-				GameObject theMole = moles[j][i];
-				string hashIndex = theMole.transform.name;
-				isJumping[hashIndex] = 0; //we use zero to indicate it is not moving.
-				//after we set it to false, why don't we also set the mole position to the minimum.
-				theMole.transform.position = new Vector3(theMole.transform.position.x,minY,theMole.transform.position.z);
-			}
-		}
-
-
+		ResetGame ();
+		hammer.transform.localPosition = targets["mole_middle_02"];
 
 		//we need to determine the max height of the moles, and the min height of them
 		//probably best to define a method that will move them up and down.
 		//maybe attach the collision handling to the hammer? It will be involved in every collision. This might be easier than attaching it to the moles.
 		//maybe we can get the name of the object we are colliding with, and if it is an active mole, then we can grant a point. If the mole isn't active, we shouldn't count it.
 
-//		subway = GameObject.Find ("subway");
-//		telephoneBooth = GameObject.Find ("photobooth");
-//		car = GameObject.Find ("racecar");
-//		airplane = GameObject.Find ("airplane");
-//		target = GameObject.Find ("groundTarget");
-//		modeLabel = (TextMesh)GameObject.Find ("modeText").GetComponent (typeof(TextMesh));
-//		scaleFactor = (TextMesh)GameObject.Find ("factorText").GetComponent (typeof(TextMesh));
-//		warning = (TextMesh)GameObject.Find ("warning").GetComponent (typeof(TextMesh));
-//		confirmed = false;
-//		
-//		location = target.transform.position;
-//		print ("Awake!!");
-//		modes = new string[3];
-//		modes [0] = "translate";
-//		modes [1] = "rotate";
-//		modes [2] = "scale";
-//		modeLabel.text = "Updated!";
-//		scale = 1.0;
-//		scaleFactor.text = "" + scale;
-//		activeMode = 0;
 	}
-	
+	void ResetGame() {
+		for(int j = 0; j< moles.Length; j++) {
+			GameObject[] row = moles[j];
+			for(int i = 0; i < row.Length; i++) {
+				//instead of using the row count, let's switch to mole name. This should be easier to check against.
+				//				string hashIndex = j.ToString () + i.ToString ();
+				GameObject theMole = moles[j][i];
+				string hashIndex = theMole.transform.name;
+				isJumping[hashIndex] = 0; //we use zero to indicate it is not moving.
+				//this sets all the moles to the start position.
+				Vector3 target = new Vector3(theMole.transform.localPosition.x,minY,theMole.transform.localPosition.z);
+				theMole.transform.localPosition = target;
+				target = new Vector3(theMole.transform.localPosition.x,theMole.transform.localPosition.y + 600,theMole.transform.localPosition.z);
+				targets[hashIndex] = target; 
+			}
+		}
+	}
 	// Update is called once per frame
 	void Update () {
-		TapSelect ();//add any necessary moles as moving
-		moveMoles ();//move the moles.
+		if (hits + 10 < jumpedCount) {
+			letsPlay = false;
+			print ("Your score: " + score.ToString ());
+		}
+		if (letsPlay) {
+			reset = false;
+			ListenForTaps();
+			TapSelect ();//add any necessary moles as moving
+			moveMoles ();//move the moles.
+			Vector3 downPosition = new Vector3(currentTarget.x,currentTarget.y - 200,currentTarget.z);
+			if(hammerDirection == 0) {
+				hammer.transform.localPosition = Vector3.Lerp (hammer.transform.localPosition,currentTarget,Time.deltaTime * hammerSpeed);
+			}
+			//if we're at our destination or in the process of going down.
+			if(hammerDirection != -999) { //use this code for finished.
+				float distance = Vector3.Distance (hammer.transform.localPosition,currentTarget);
+				if((distance < 1.0f) || (hammerDirection == -1)) {
+//					print ("moving down!");
+					hammerDirection = -1;
+					hammer.transform.localPosition = Vector3.Lerp (hammer.transform.localPosition,downPosition,Time.deltaTime * strikingSpeed);
+				} 
+				distance = Vector3.Distance (hammer.transform.localPosition,downPosition);
+				if ((distance < 1.0f) || (hammerDirection == 1)) {
+					hammerDirection = 1;
+					hammer.transform.localPosition = Vector3.Lerp (hammer.transform.localPosition,currentTarget,Time.deltaTime * strikingSpeed * 0.66f);
+					distance = Vector3.Distance (hammer.transform.localPosition,currentTarget);
+					if(distance < 1.1f) {
+						hammerDirection = -999;
+//						print("finished!");
+					}
+				}
+
+			} else {
+				//we finished with the current target. Set the next target so it will get lerped to.
+//				print ("now get the next target!");
+			}
+		} else {
+			ListenForTaps();
+		}
+			//else if (!reset){
+			//ResetGame();
+			//reset = true;
+		//}
 	}
-//	void switchMode() {
-//		if (activeMode < modes.Length - 1) {
-//			activeMode ++;
-//		} else {
-//			activeMode = 0;
-//		}
-//		modeLabel.text = modes [activeMode];
-//	}
+
 	void moveMoles() {
 //		print ("trying to move!");
 //		print (jumpingMoles.Count.ToString () + " jumping moles!");
@@ -130,27 +157,28 @@ public class mole_behavior : MonoBehaviour {
 				string moleName = mole.transform.name;
 				int direction = isJumping [moleName];
 //				print ("Mole position:" + direction.ToString ());
-				float speed = 50.0f;
+				float speed = 200.0f;
 				//		int initialY = mole.transform.Translate.Y;
 				if ((direction == 1) || (direction == -1)) {
 //				print ("is anyone there??");
 						//move it in the designated direction
 //						mole.transform.Translate (Vector3.up * Time.deltaTime * direction);
-						float newPosition = mole.transform.position.y + (Time.deltaTime * direction * speed);
-						mole.transform.position = new Vector3(mole.transform.position.x,newPosition,mole.transform.position.z);
-						if ((direction == -1) && (mole.transform.position.y < minY)) {
+						float newPosition = mole.transform.localPosition.y + (Time.deltaTime * direction * speed);
+						mole.transform.localPosition = new Vector3(mole.transform.localPosition.x,newPosition,mole.transform.localPosition.z);
+						if ((direction == -1) && (mole.transform.localPosition.y < minY)) {
 								//time to make the mole stop moving.
-							print ("finished animating a mole!");
+//							print ("finished animating a mole!");
+							jumpedCount++;
 							isJumping [moleName] = 0;
 
 //					print(jumpingMoles.Count.ToString ());
 //							jumpingMoles.Remove (mole); //take it out of the list.
 							toRemove.Add (mole);
-							mole.transform.position = new Vector3(mole.transform.position.x, minY, mole.transform.position.z);
+						mole.transform.localPosition = new Vector3(mole.transform.localPosition.x, minY, mole.transform.localPosition.z);
 //					print (jumpingMoles.Count.ToString ());
 
 
-						} else if((direction == 1) && (mole.transform.position.y > maxY)) {
+						} else if((direction == 1) && (mole.transform.localPosition.y > maxY)) {
 							//we need to start the delay.
 							direction++;
 							isJumping[moleName] = direction;
@@ -172,69 +200,6 @@ public class mole_behavior : MonoBehaviour {
 		}
 //		if(mole.transform
 	}
-	
-//	void buttonPressed(string which) {
-//		modeLabel.text = which;
-//		switch (which) {
-//		case "+x":
-//			adjustPositioning("x",false);
-//			break;
-//		case "+y":
-//			adjustPositioning ("y",false);
-//			break;
-//		case "+z":
-//			adjustPositioning("z",false);
-//			break;
-//		case "-x":
-//			adjustPositioning("x",true);
-//			break;
-//		case "-y":
-//			adjustPositioning("y",true);
-//			break;
-//		case "-z":
-//			adjustPositioning ("z",true);
-//			break;
-//		case "+factor":
-//			scale = scale * 1.5;
-//			scaleFactor.text = "" + scale;
-//			break;
-//		case "-factor":
-//			scale = scale * 0.6666667;
-//			scaleFactor.text = "" + scale;
-//			break;
-//		case "toggleMode":
-//			switchMode();
-//			break;
-//		case "clone":
-//			Behaviour h = (Behaviour)selected.GetComponent ("Halo");
-//			h.enabled = false;
-//			GameObject newObject = (GameObject)Instantiate(selected);
-//			selected = newObject;
-//			newObject.transform.position = location;
-//			newObject.transform.parent = target.transform;
-//			float currentScale = newObject.transform.localScale.x * 10;
-//			newObject.transform.localScale = new Vector3(currentScale,currentScale,currentScale);
-//			h = (Behaviour)selected.GetComponent ("Halo");
-//			h.enabled = true;
-//			break;
-//		case "delete":
-//			if(confirmed) {
-//				selected.SetActive(false);
-//				warning.text = "";
-//				confirmed = false;
-//			} else {
-//				warning.text = "Are you sure?";
-//				confirmed = true;
-//			}
-//			break;
-//		}
-//		if (which != "delete") {
-//			//in the event that we didn't choose delete, make sure confirmed is set to false.
-//			confirmed = false;
-//			warning.text = "";
-//		}
-//		
-//	}
 	
 //	void adjustPositioning(string axis, bool isNegative) {
 //		if (selected) {
@@ -284,15 +249,15 @@ public class mole_behavior : MonoBehaviour {
 		//we'll want to be able to randomly select a row, and only initiate a mole jump if we don't have too many moles jumping already.
 		//maybe we need to keep an array list of the moles that are moving?
 		int moveAMole = Random.Range (0, 100);
-		if ((moveAMole > jumpThreshold) && (jumpingMoles.Count < 4)) {
+		if ((moveAMole > jumpThreshold) && (jumpingMoles.Count < maxMoles)) {
 			foreach(GameObject[] row in moles) {
 				foreach (GameObject mole in row) {
 					int moveThisMole = Random.Range (0,100);
-					if((moveThisMole > jumpThreshold) && (jumpingMoles.Count < 4)) {
+					if((moveThisMole > jumpThreshold) && (jumpingMoles.Count < maxMoles)) {
 						if(isJumping[mole.transform.name] == 0) {
 							//let's move the mole slightly below the minimum in order to signal that it will move up.
 							//this is for user experience :)
-//							mole.transform.position = new Vector3(mole.transform.position.x,mole.transform.position.y - 20, mole.transform.position.z);
+							mole.transform.localPosition = new Vector3(mole.transform.localPosition.x,mole.transform.localPosition.y - 20, mole.transform.localPosition.z);
 							isJumping[mole.transform.name] = 1; //move it up.
 							jumpingMoles.Add(mole);
 //							moveMole(mole);
@@ -302,87 +267,58 @@ public class mole_behavior : MonoBehaviour {
 				}
 			}
 		}
-
-//		bool alreadyChanged = false;
-//		if (Input.GetMouseButtonDown (0)) {
-//			print ("tapped something!!");
-//			//also make sure confirmed is false here.
-//			confirmed = false;
-//			warning.text = "";
-//			Ray ray = Camera.main.ScreenPointToRay (Input.mousePosition);
-//			RaycastHit hit;
-//			if(Physics.Raycast (ray,out hit, 1000)) {
-//				Behaviour h;
-//				string objectName = hit.collider.gameObject.name;
-//				if(selected) { //if we already selected something.
-//					h = (Behaviour)selected.GetComponent ("Halo");
-//					h.enabled = false; // turn off the old one.
-//				}
-//				if(selected && selected.name.Equals(objectName)) {
-//					count++;
-//					if(!alreadyChanged) {
-//						alreadyChanged = true;
-//						switchMode ();
-//					}
-//				} else {
-//					count = 1;
-//					activeMode = 0;
-//				}
-//				scaleFactor.text = "" + count;
-//				selected = hit.collider.gameObject; //assign the new one
-//				h = (Behaviour)selected.GetComponent ("Halo");
-//				if(!h.enabled) {
-//					h.enabled = true;//turn on the halo.
-//				} 
-//				//				GameObject newObject = (GameObject)Instantiate(airplane);
-//				//				newObject.transform.position = location;
-//				//				newObject.transform.parent = target.transform;
-//				//				float currentScale = newObject.transform.localScale.x * 10;
-//				//				newObject.transform.localScale = new Vector3(currentScale,currentScale,currentScale);
-//				//				//				newObject.
-//				//				hit.collider.gameObject.transform.Translate (Vector3.left * Time.deltaTime);
-//				//				print(objectName);
-//				//				modeLabel.text = objectName;
-//			} else {
-//				//we didn't hit anything. Let's turn off the selection.
-//				if(selected) {
-//					Behaviour h = (Behaviour)selected.GetComponent ("Halo");
-//					h.enabled = false;
-//				}
-//			}
-//			
-//		}
-//		foreach (Touch touch in Input.touches) {
-//			Ray ray = Camera.main.ScreenPointToRay (touch.position);
-//			//also make sure confirmed is false here.
-//			confirmed = false;
-//			warning.text = "";
-//			RaycastHit hit;
-//			if(Physics.Raycast (ray,out hit, 1000)) {
-//				Behaviour h;
-//				string objectName = hit.collider.gameObject.name;
-//				if(selected.name.Equals(objectName)) {
-//					if(!alreadyChanged) {
-//						alreadyChanged = true;
-//						switchMode ();
-//					}
-//					count++;
-//				} else {
-//					count = 1;
-//				}
-//				modeLabel.text = objectName;
-//				//				scaleFactor.text = "" + count;
-//				if(selected) { //if we already selected something.
-//					h = (Behaviour)selected.GetComponent ("Halo");
-//					h.enabled = false; // turn off the old one.
-//				}
-//				selected = hit.collider.gameObject; //assign the new one
-//				h = (Behaviour)selected.GetComponent ("Halo");
-//				if(!h.enabled) {
-//					h.enabled = true;//turn on the halo.
-//				} 
-//			}
-//		}
-//		
 	}
+
+	public void reportCollidedWith(string name) {
+		hits++;
+		score += 100;
+		yourScoreboard.text = score.ToString ();
+		print ("inside mole_behavior!");
+		print (name);
+		hammerDirection = 1;//go back up, we hit something!
+
+	}
+
+	void ListenForTaps() {
+		if (Input.GetMouseButtonDown (0)) {
+//			print ("tapped something!!");
+			Ray ray = Camera.main.ScreenPointToRay (Input.mousePosition);
+			RaycastHit hit;
+			if(Physics.Raycast (ray,out hit, 20000)) {
+				Behaviour h;
+				string objectName = hit.collider.gameObject.name;
+				if(objectName == "lets_play") {
+					letsPlay = true;
+				} else if(letsPlay) {
+					if(objectName != "mole_hammer") {
+						currentTarget = targets[objectName];
+						hammerDirection = 0;
+					}
+				}
+
+//				hammer.transform.localPosition = targets[objectName];
+			} else {
+				//we didn't hit anything. Let's turn off the selection.
+//				print ("no collision!");
+			}
+			
+		}
+		foreach (Touch touch in Input.touches) {
+			Ray ray = Camera.main.ScreenPointToRay (touch.position);
+			RaycastHit hit;
+			if(Physics.Raycast (ray,out hit, 20000)) {
+				Behaviour h;
+				string objectName = hit.collider.gameObject.name;
+				if(objectName == "lets_play") {
+					letsPlay = true;
+				} else if (letsPlay) {
+					if(objectName != "mole_hammer") {
+						currentTarget = targets[objectName];
+						hammerDirection = 0;
+					}
+				}
+			}
+		}
+	}
+
 }
